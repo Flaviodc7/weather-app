18d2c30f5c98d129de1a4fce298bb4a6d2901697
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-17 17:47:20 -0300

    Eliminando archivos .js y agregando estilos

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <h1 className="head">
+      <span className="light">Wheater</span> Forecast
+    </h1>
+  )
+}
+
+export default Header
---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Header from "../Header/Header";
+import "./Page.css";
+
+const Page = () => {
+  return (
+    <Fragment>
+      <Header />
+      <div className=""></div>
+    </Fragment>
+  );
+};
+
+export default Page;
