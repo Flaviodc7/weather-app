94f253dc94cccb1ac64256cff9c21ffd42b837f9
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-21 09:18:19 -0300

    Agregando estilos y funcionalidad

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 const Form = () => {
+  const [location, setLocation] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!location || location === "") return;
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         aria-label="location"
         type="text"
         className="input form-control"
         placeholder="Buscar una ubicación"
         required
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
       />
-      <button type="submit" className="button">
-          Buscar
+      <button type="submit" className="button" onClick={onSubmit}>
+        BUSCAR
       </button>
     </form>
   );
