6fc41bda6ca99f328872f2ff22e27b9a4efb3fda
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-21 09:38:13 -0300

    Creando Custom Hook y enlazando

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,14 +1,23 @@
 import React, { Fragment } from "react";
 import Header from "../Header/Header";
-import Form from "../Form/Form"
+import Form from "../Form/Form";
+import Error from "../Error/Error"
+import Loader from "../Loader/Loader"
+import Forecast from "../Forecast/Forecast"
 import "./Page.css";
 
+import useForecast from "../../hooks/useForecast";
+
 const Page = () => {
+  const {isError, isLoading, forecast} = useForecast();
   return (
     <Fragment>
       <Header />
       <div className="box">
         <Form />
+        {isError && <Error />}
+        {isLoading && <Loader />}
+        {forecast && <Forecast />}
       </div>
     </Fragment>
   );
