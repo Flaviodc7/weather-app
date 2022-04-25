08adf23f6ad3fd4e8ab12e5287801cbc003a341c
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-25 14:44:56 -0300

    Agregando código a CurrentDayDescriptionItem

---
+++ src/components/currentDayDescriptionItem/CurrentDayDescriptionItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CurrentDayDescriptionItem = () => {
+  return (
+    <div className="d-flex justify-content-between">
+        <p className="mb-0 font-weight-bolder text-uppercase"></p>
+        <p className="mb-0"></p>
+    </div>
+  )
+}
+
+export default CurrentDayDescriptionItem
