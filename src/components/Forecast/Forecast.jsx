67cc36d7d0b157180f93ee0408f9fe82c0eaf08b
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-25 15:00:44 -0300

    Agregando contenido a Forecast

---
+++ src/components/Forecast/Forecast.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+import './Forecast.css'
+
+const Forecast = () => {
+  return (
+    <Container className="box">
+        <Row>
+            <Col xs={12} md={4}>
+                <div className="card"></div>
+            </Col>
+            <Col xs={12} md={8} className="d-flex flex-column justify-content-between"></Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default Forecast
