6b42718f80f1656075fc747d13cc9b645ca6c3a3
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-26 14:08:51 -0300

    Agregando UpcomingDaysForecast

---
+++ src/components/UpcomingDaysForecast/UpcomingDaysForecast.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './UpcomingDaysForecast.css'
+
+const UpcomingDaysForecast = () => {
+  return (
+    <ul className="weekList d-flex justify-content-between p-0"></ul>
+  )
+}
+
+export default UpcomingDaysForecast
