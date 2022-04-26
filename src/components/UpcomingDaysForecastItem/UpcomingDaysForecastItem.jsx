711552afa57d470187b2862f993875e6a14b7bfe
Flavio Coscarella <flaviodc@live.com.ar>
2022-04-26 14:14:38 -0300

    Añadiendo UpcomingDaysForecastItem

---
+++ src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./UpcomingDaysForecastItem.css";
+
+const UpcomingDaysForecastItem = () => {
+  return (
+    <li className="weekday d-flex flex-column justify-content-center align-items-center p-2"></li>
+  );
+};
+
+export default UpcomingDaysForecastItem;
